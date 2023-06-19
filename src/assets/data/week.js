6ea79b044f233f4a778a8b476dcf75943d0530dc
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2023-06-19 10:02:10 +0200

    Create weekdays datas array file

---
+++ src/assets/data/week.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 0,
+    weekDay: 'sunday'
+  },
+  {
+    id: 1,
+    weekDay: 'monday'
+  },
+  {
+    id: 2,
+    weekDay: 'tuesday'
+  },
+  {
+    id: 3,
+    weekDay: 'wednesday'
+  },
+  {
+    id: 4,
+    weekDay: 'thursday'
+  },
+  {
+    id: 5,
+    weekDay: 'friday'
+  },
+  {
+    id: 6,
+    weekDay: 'saturday'
+  }
+]
