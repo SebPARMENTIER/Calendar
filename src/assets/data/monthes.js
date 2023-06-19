a37a05bd7bff3e06fa1a45aa30a2363483db88b7
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2023-06-19 10:00:05 +0200

    Create monthes datas array file

---
+++ src/assets/data/monthes.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 0,
+    month: 'January',
+    maxDay: 31
+  },
+  {
+    id: 1,
+    month: 'February',
+    maxDay: 28
+  },
+  {
+    id: 2,
+    month: 'March',
+    maxDay: 31
+  },
+  {
+    id: 3,
+    month: 'April',
+    maxDay: 30
+  },
+  {
+    id: 4,
+    month: 'May',
+    maxDay: 31
+  },
+  {
+    id: 5,
+    month: 'June',
+    maxDay: 30
+  },
+  {
+    id: 6,
+    month: 'July',
+    maxDay: 31
+  },
+  {
+    id: 7,
+    month: 'August',
+    maxDay: 31
+  },
+  {
+    id: 8,
+    month: 'September',
+    maxDay: 30
+  },
+  {
+    id: 9,
+    month: 'October',
+    maxDay: 31
+  },
+  {
+    id: 10,
+    month: 'November',
+    maxDay: 30
+  },
+  {
+    id: 11,
+    month: 'December',
+    maxDay: 31
+  }
+]
